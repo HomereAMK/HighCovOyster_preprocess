8b76eb41334976e75ecffb39484825e2e61486a6
HomereAMK <hmon@aqua.dtu.dk>
2022-06-28 22:17:18 +0200

    h

--- 00_scripts/07_depthFastq_launch.sh
+++ 00_scripts/07_depthFastq_launch.sh
@@ -4,7 +4,7 @@
 rm 00_scripts/DEPTHFASTQ*sh
 
 # launch scripts for Colosse
-for file in $(ls/home/projects/dp_00007/people/hmon/HighCovOyster_preprocess/02_data*.fq.gz|sed -e 's/_[12].fq.gz//'|sort -u)    #or #for file in $(ls 02_data/*fastq.gz|perl -pe 's/.f(ast)?q.gz//'|sort -u)
+for file in $(ls /home/projects/dp_00007/people/hmon/HighCovOyster_preprocess/02_data*.fq.gz|sed -e 's/_[12].fq.gz//'|sort -u)    #or #for file in $(ls 02_data/*fastq.gz|perl -pe 's/.f(ast)?q.gz//'|sort -u)
 
 do
 	base=$(basename "$file")
